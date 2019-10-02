fe1f95bbd0c59964f9e17ce5b67ce9aaebc05ace
mohsenkhadem1 <mohsen.khadem@ed.ac.uk>
2019-10-02 11:08:08 +0100

    Updated to connect to openCV

--- src/image_processing.py
+++ src/image_processing.py
@@ -26,78 +26,6 @@ class image_converter:
     # initialize a subscriber to recieve messages rom a topic named /robot/camera1/image_raw and use callback function to recieve data
     self.image_sub = rospy.Subscriber("/robot/camera1/image_raw",Image,self.callback)
 
-
-  # In this method you can focus on detecting the centre of the red circle
-  def detect_red(self,image):
-      # Isolate the blue colour in the image as a binary image
-      mask = cv2.inRange(image, (0, 0, 100), (0, 0, 255))
-      # This applies a dilate that makes the binary region larger (the more iterations the larger it becomes)
-      kernel = np.ones((5, 5), np.uint8)
-      mask = cv2.dilate(mask, kernel, iterations=3)
-      # Obtain the moments of the binary image
-      M = cv2.moments(mask)
-      # Calculate pixel coordinates for the centre of the blob
-      cx = int(M['m10'] / M['m00'])
-      cy = int(M['m01'] / M['m00'])
-      return np.array([cx, cy])
- 
-
-  # Detecting the centre of the green circle
-  def detect_green(self,image):
-      mask = cv2.inRange(image, (0, 100, 0), (0, 255, 0))
-      kernel = np.ones((5, 5), np.uint8)
-      mask = cv2.dilate(mask, kernel, iterations=3)
-      M = cv2.moments(mask)
-      cx = int(M['m10'] / M['m00'])
-      cy = int(M['m01'] / M['m00'])
-      return np.array([cx, cy])
-
-
-  # Detecting the centre of the blue circle
-  def detect_blue(self,image):
-      mask = cv2.inRange(image, (100, 0, 0), (255, 0, 0))
-      kernel = np.ones((5, 5), np.uint8)
-      mask = cv2.dilate(mask, kernel, iterations=3)
-      M = cv2.moments(mask)
-      cx = int(M['m10'] / M['m00'])
-      cy = int(M['m01'] / M['m00'])
-      return np.array([cx, cy])
-
-  # Detecting the centre of the yellow circle
-  def detect_yellow(self,image):
-      mask = cv2.inRange(image, (0, 100, 100), (0, 255, 255))
-      kernel = np.ones((5, 5), np.uint8)
-      mask = cv2.dilate(mask, kernel, iterations=3)
-      M = cv2.moments(mask)
-      cx = int(M['m10'] / M['m00'])
-      cy = int(M['m01'] / M['m00'])
-      return np.array([cx, cy])
-
-
-  # Calculate the conversion from pixel to meter
-  def pixel2meter(self,image):
-      # Obtain the centre of each coloured blob
-      circle1Pos = self.detect_blue(image)
-      circle2Pos = self.detect_green(image)
-      # find the distance between two circles
-      dist = np.sum((circle1Pos - circle2Pos)**2)
-      return 3 / np.sqrt(dist)
-
-
-    # Calculate the relevant joint angles from the image
-  def detect_joint_angles(self,image):
-    a = self.pixel2meter(image)
-    # Obtain the centre of each coloured blob 
-    center = a * self.detect_yellow(image)
-    circle1Pos = a * self.detect_blue(image) 
-    circle2Pos = a * self.detect_green(image) 
-    circle3Pos = a * self.detect_red(image)
-    # Solve using trigonometry
-    ja1 = np.arctan2(center[0]- circle1Pos[0], center[1] - circle1Pos[1])
-    ja2 = np.arctan2(circle1Pos[0]-circle2Pos[0], circle1Pos[1]-circle2Pos[1]) - ja1
-    ja3 = np.arctan2(circle2Pos[0]-circle3Pos[0], circle2Pos[1]-circle3Pos[1]) - ja2 - ja1
-    return np.array([ja1, ja2, ja3])
-  
   # Recieve data, process it, and publish
   def callback(self,data):
     # Recieve the image
@@ -112,14 +40,14 @@ class image_converter:
     # Uncomment if you want to save the image
     #cv2.imwrite('image_copy.png', cv_image)
 
-    a = self.detect_joint_angles(cv_image)
     cv2.imshow('window', cv_image)
     cv2.waitKey(3)
 
+    # change te value of self.joint.data to your estimated value from thew images once you have finalized the code
     self.joints = Float64MultiArray()
-    self.joints.data = a
+    self.joints.data = np.array([0, 0, 0])
 
-    # Publish the results
+    # Publish the results - the images are published under a topic named "image_topic" and calculated joints angles are published under a topic named "joints_pos"
     try:
       self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
       self.joints_pub.publish(self.joints)
