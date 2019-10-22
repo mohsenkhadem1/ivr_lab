75c8cbc68c7e471dc0406f4ee6a9f8accb6daf57
mohsenkhadem1 <39303711+mohsenkhadem1@users.noreply.github.com>
2019-10-22 18:03:51 +0100

    Update image_processing.py

--- src/image_processing.py
+++ src/image_processing.py
@@ -7,7 +7,7 @@ import cv2
 import numpy as np
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
-from std_msgs.msg import Float64MultiArray
+from std_msgs.msg import Float64MultiArray, Float64
 from cv_bridge import CvBridge, CvBridgeError
 
 
@@ -21,11 +21,29 @@ class image_converter:
     self.image_pub = rospy.Publisher("image_topic",Image, queue_size = 1)
     # initialize a publisher to send joints' angular position to a topic called joints_pos
     self.joints_pub = rospy.Publisher("joints_pos",Float64MultiArray, queue_size=10)
+    # initialize a publisher to send robot end-effector position
+    self.end_effector_pub = rospy.Publisher("end_effector_prediction",Float64MultiArray, queue_size=10)
+    # initialize a publisher to send desired trajectory
+    self.trajectory_pub = rospy.Publisher("trajectory",Float64MultiArray, queue_size=10)
+    # initialize a publisher to send joints' angular position to the robot
+    self.robot_joint1_pub = rospy.Publisher("/robot/joint1_position_controller/command", Float64, queue_size=10)
+    self.robot_joint2_pub = rospy.Publisher("/robot/joint2_position_controller/command", Float64, queue_size=10)
+    self.robot_joint3_pub = rospy.Publisher("/robot/joint3_position_controller/command", Float64, queue_size=10)
     # initialize the bridge between openCV and ROS
     self.bridge = CvBridge()
     # initialize a subscriber to recieve messages rom a topic named /robot/camera1/image_raw and use callback function to recieve data
     self.image_sub = rospy.Subscriber("/robot/camera1/image_raw",Image,self.callback)
-
+    # record the begining time
+    self.time_trajectory = rospy.get_time()
+  
+  # Define a circular trajectory (for lab 3)
+  def trajectory(self):
+    # get current time
+    cur_time = np.array([rospy.get_time() - self.time_trajectory])
+    x_d = float(6* np.cos(cur_time * np.pi/100))
+    y_d = float(6 + np.absolute(1.5* np.sin(cur_time * np.pi/100)))
+    return np.array([x_d, y_d])
+  
   # Recieve data, process it, and publish
   def callback(self,data):
     # Recieve the image
@@ -53,6 +71,37 @@ class image_converter:
     self.joints = Float64MultiArray()
     self.joints.data = np.array([0, 0, 0])
 
+    # publish the estimated position of robot end-effector (for lab 3)
+    #x_e_image = np.array([0, 0, 0])
+    #self.end_effector=Float64MultiArray()
+    #self.end_effector.data= x_e_image	
+
+    # send control commands to joints (for lab 3)
+    #self.joint1=Float64()
+    #self.joint1.data= q_d[0]
+    #self.joint2=Float64()
+    #self.joint2.data= q_d[1]
+    #self.joint3=Float64()
+    #self.joint3.data= q_d[2]
+
+    # Publishing the desired trajectory on a topic named trajectory(for lab 3)
+    x_d = self.trajectory()    # getting the desired trajectory
+    self.trajectory_desired= Float64MultiArray()
+    self.trajectory_desired.data=x_d
+
+    # Publish the results
+    try: 
+      self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+      self.joints_pub.publish(self.joints)
+      #(for lab 3)
+      #self.trajectory_pub.publish(self.trajectory_desired)
+      #self.end_effector_pub.publish(self.end_effector)
+      #self.robot_joint1_pub.publish(self.joint1)
+      #self.robot_joint2_pub.publish(self.joint2)
+      #self.robot_joint3_pub.publish(self.joint3)
+    except CvBridgeError as e:
+      print(e)
+      
     # Publish the results - the images are published under a topic named "image_topic" and calculated joints angles are published under a topic named "joints_pos"
     try:
       self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
